4c238843513bd0520bd82a14707a9eccc055a6b0
ZoroPinus <zoropinus@gmail.com>
2024-04-02 12:02:53 +0800

    Update layout.tsx

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -4,29 +4,22 @@ import Link from "next/link";
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="relative h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+      {/* Left */}
       <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
         <div className="absolute inset-0 bg-zinc-900" />
-        <div className="relative z-20 flex items-center text-2xl font-medium">
-          {/* <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className="mr-2 h-6 w-6"
-          >
-            <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
-          </svg> */}
-          <Image
-            width={170}
-            height={170}
-            alt="Website logo"
-            src="https://utfs.io/f/d3fdfc03-cf46-467b-86e5-613e6a6e7acf-2a.png"
-          />
+        <div className="relative z-20 flex items-center justify-center h-full">
+          <div className="flex flex-col items-center">
+            <Image
+              width={170}
+              height={170}
+              alt="Website logo"
+              src="https://utfs.io/f/d3fdfc03-cf46-467b-86e5-613e6a6e7acf-2a.png"
+            />
+            <h1 className="text-2xl font-medium text-white mt-2">System Title</h1>
+          </div>
         </div>
       </div>
+      {/* Right */}
       <div className="p-4 lg:p-8 h-full flex items-center">
         <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
           <div className="flex flex-col space-y-2 text-center">
@@ -59,9 +52,6 @@ const AuthLayout = ({ children }: { children: React.ReactNode }) => {
       </div>
     </div>
 
-    // <div className="h-full flex items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
-    //   {children}
-    // </div>
   );
 };
 
