eee0d5d1d6ab553a9ea06b1a234539ac14a8ac25
ZoroPinus <zoropinus@gmail.com>
2024-04-02 11:17:23 +0800

    Update layout.tsx

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -6,7 +6,7 @@ const AuthLayout = ({ children }: { children: React.ReactNode }) => {
     <div className="relative h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
       <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
         <div className="absolute inset-0 bg-zinc-900" />
-        <div className="relative z-20 flex items-center text-lg font-medium">
+        <div className="relative z-20 flex items-center text-2xl font-medium">
           {/* <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 24 24"
@@ -20,8 +20,8 @@ const AuthLayout = ({ children }: { children: React.ReactNode }) => {
             <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
           </svg> */}
           <Image
-            width={20}
-            height={20}
+            width={170}
+            height={170}
             alt="Website logo"
             src="https://utfs.io/f/d3fdfc03-cf46-467b-86e5-613e6a6e7acf-2a.png"
           />
