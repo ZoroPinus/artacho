7f995f1938035f99149c1b860c35bbe23473dec9
ZoroPinus <zoropinus@gmail.com>
2024-04-10 21:06:51 +0800

    Update page.tsx

--- app/(protected)/dashboard/page.tsx
+++ app/(protected)/dashboard/page.tsx
@@ -83,7 +83,7 @@ const DashboardPage = () => {
                   <Card>
                     <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
                       <CardTitle className="text-sm font-medium">
-                        Total Members
+                        Regular Members
                       </CardTitle>
                       {/* USERS ICON */}
                       <svg
